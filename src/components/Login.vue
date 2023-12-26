738fb03aad8f109fb281fd3e1a48d67d7d29002f
BSIT2A-abas <rabas_220000001580@uic.edu.ph>
2023-12-27 00:51:34 +0800

    modified:   src/components/Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="logoContainer">
-        <img src="src/assets/POSLOGO.jpg" alt="Image" width="250" />
+        <img src="../assets/POSLOGO.jpg" alt="Image" width="250" />
         </div> 
         <div class="formContainer">
             <div class="inputContainer">
